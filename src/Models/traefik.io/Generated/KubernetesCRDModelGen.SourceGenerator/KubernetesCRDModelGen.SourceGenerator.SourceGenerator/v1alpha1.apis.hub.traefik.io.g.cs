7c4f3fb66d4c171c80bddb6e75463702caa71e50
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-09-07 11:16:11 +0000

    feat: Sync

--- src/Models/traefik.io/Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.apis.hub.traefik.io.g.cs
+++ src/Models/traefik.io/Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.apis.hub.traefik.io.g.cs
@@ -40,6 +40,29 @@ public partial class V1alpha1APIList : IKubernetesObject<V1ListMeta>, IItems<V1a
     public required IList<V1alpha1API> Items { get; set; }
 }
 
+/// <summary>OperationFilter specifies the allowed operations on APIs and APIVersions.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.10+a22b941414add0bcc94c90de54d985f643c33be0")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APISpecApiAuthsOperationFilter
+{
+    /// <summary>Include defines the names of OperationSets that will be accessible.</summary>
+    [JsonPropertyName("include")]
+    public IList<string>? Include { get; set; }
+}
+
+/// <summary>APIAuthReference references an APIAuth resource for authentication configuration.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.10+a22b941414add0bcc94c90de54d985f643c33be0")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APISpecApiAuths
+{
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    /// <summary>OperationFilter specifies the allowed operations on APIs and APIVersions.</summary>
+    [JsonPropertyName("operationFilter")]
+    public V1alpha1APISpecApiAuthsOperationFilter? OperationFilter { get; set; }
+}
+
 /// <summary>Cors defines the Cross-Origin Resource Sharing configuration.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.10+a22b941414add0bcc94c90de54d985f643c33be0")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
@@ -197,6 +220,10 @@ public partial class V1alpha1APISpecVersions
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1APISpec
 {
+    /// <summary>APIAuths defines the API authentication configuration.</summary>
+    [JsonPropertyName("apiAuths")]
+    public IList<V1alpha1APISpecApiAuths>? ApiAuths { get; set; }
+
     /// <summary>Cors defines the Cross-Origin Resource Sharing configuration.</summary>
     [JsonPropertyName("cors")]
     public V1alpha1APISpecCors? Cors { get; set; }
